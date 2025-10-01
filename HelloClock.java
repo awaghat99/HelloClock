c91ff203cb68b54d9786da7dfb9446006ae70024
Aadil Waghat <a.waghat16@gmail.com>
2025-10-01 15:12:05 +0100

    add multiple names and exit logic

--- HelloClock.java
+++ HelloClock.java
@@ -7,19 +7,31 @@ public class HelloClock {
         System.out.println("Welcome to hello clock!");
 
         LocalTime now = LocalTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm a");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm:ss");
         String cleanTime = now.format(formatter);
         System.out.println("The current time is: " + cleanTime);
 
 
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("What is your name: ");
-        String name = sc.nextLine();
+        String name = "";
+
+        while (!name.equals("exit")) {
+            System.out.println("What is your name: ");
+            name = sc.nextLine();
+            LocalTime timeForPerson = LocalTime.now();
+            cleanTime = timeForPerson.format(formatter); 
+            if (!name.equals("exit")) {
+                System.out.println("Hello " + name + "! The time you joined was: " + cleanTime);
+            }
+            
+            
+        }
+        
 
         sc.close();
 
-        System.out.println("Hello " + name + "! The time you joined was: " + cleanTime);
+        
 
 
 
